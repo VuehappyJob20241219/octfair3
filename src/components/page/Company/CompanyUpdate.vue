6b66f57cd6fe73676e24fcdf105d0dc659541d7e
youngchan <yckim8721@gmail.com>
2024-12-19 17:26:43 +0900

    company 초기커밋

---
+++ src/components/page/Company/CompanyUpdate.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <table>
+      <colgroup>
+        <col width="18%" />
+        <col width="" />
+        <col width="18%" />
+        <col width="" />
+      </colgroup>
+      <tbody>
+        <tr>
+          <th>사업자명</th>
+          <td>
+            <input type="text" />
+          </td>
+          <th>사업자 대표</th>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <th>연락처</th>
+          <td>
+            <input type="text" />
+          </td>
+          <th>사업자 주소</th>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <th>사원수</th>
+          <td>
+            <input type="text" />
+          </td>
+          <th>홈페이지 주소</th>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <th>설립일</th>
+          <td>
+            <input type="text" />
+          </td>
+          <th>매출액</th>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <th>기업소개</th>
+          <td></td>
+        </tr>
+        <tr>
+          <th>기업로고</th>
+          <td></td>
+        </tr>
+        <tr>
+          <th>미리보기</th>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+    <div>
+      <button>등록</button>
+      <button>수정</button>
+      <button>삭제</button>
+      <button>돌아가기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
