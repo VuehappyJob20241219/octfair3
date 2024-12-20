f7cac09067d831c766aba9132f6fccf3f8303f2a
psj <poker6711@naver.com>
2024-12-20 12:06:09 +0900

    공고리스트(기업) css제외 완료

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,8 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    BizPostMain: typeof import('./src/components/page/ManageHire/BizPost/BizPostMain.vue')['default']
+    BizPostSearch: typeof import('./src/components/page/ManageHire/BizPost/BizPostSearch.vue')['default']
     ContextBox: typeof import('./src/components/common/ContextBox.vue')['default']
     copy: typeof import('./src/components/page/Board/Notice/FirstNoticeMain copy.vue')['default']
     FirstNoticeDetail: typeof import('./src/components/page/Board/Notice/FirstNoticeDetail.vue')['default']
