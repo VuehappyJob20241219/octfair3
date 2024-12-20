47f74be0747cf328e350d7e01a891860a68da471
hyeyum <top2480@naver.com>
2024-12-20 15:17:52 +0900

    faq 오류잡기기

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,11 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ApplicantMain: typeof import('./src/components/page/Manage-user/ApplicantMain.vue')['default']
+    ApplicantSearch: typeof import('./src/components/page/Manage-user/ApplicantSearch.vue')['default']
+    BizPostDetail: typeof import('./src/components/page/ManageHire/BizPost/BizPostDetail.vue')['default']
+    BizPostInsert: typeof import('./src/components/page/ManageHire/BizPost/BizPostInsert.vue')['default']
+    BizPostMain: typeof import('./src/components/page/ManageHire/BizPost/BizPostMain.vue')['default']
     ContextBox: typeof import('./src/components/common/ContextBox.vue')['default']
     copy: typeof import('./src/components/page/Board/Notice/FirstNoticeMain copy.vue')['default']
     FaqMain: typeof import('./src/components/page/Board/Faq/FaqMain.vue')['default']
@@ -25,6 +30,7 @@ declare module 'vue' {
     NoticeModal: typeof import('./src/components/page/Board/Notice/NoticeModal.vue')['default']
     NoticeSearch: typeof import('./src/components/page/Board/Notice/NoticeSearch.vue')['default']
     Pagination: typeof import('./src/components/common/Pagination.vue')['default']
+    ResumeList: typeof import('./src/components/page/Apply/ResumeMain/ResumeList.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
