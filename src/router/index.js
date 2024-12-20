f4f51cf4e0fc18ef1652f35336f44910586e1b5b
hyeyum <top2480@naver.com>
2024-12-20 15:17:52 +0900

    faq 오류잡기기

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Login/Login.vue";
 import DashBoard from "../views/DashBoard/DashBoard.vue";
 import Notice from "../views/Board/Notice.vue";
+import Post from "../views/ManageHire/Post.vue";
 import FirstNoticeDetail from "../components/page/Board/Notice/FirstNoticeDetail.vue";
+import Applicant from "../views/Manage-user/Applicant.vue";
+import Resume from "../views/Apply/Resume.vue";
 import Faq from "../views/Board/Faq.vue";
 
 const routes = [
@@ -43,6 +46,40 @@ const routes = [
             component: Faq,
           }
         ],
+      },
+      {
+        path: "manage-user",
+        name: "manage-user",
+        children: [
+          {
+            path: "applicant.do",
+            name: "applicant",
+            component: Applicant,
+          },
+        ],
+      },
+      {
+        path: "apply",
+        name: "apply",
+        children: [
+          {
+            path: "resume.do",
+            name: "resume",
+            component: Resume,
+          },
+        ],
+      },
+      {
+        path: "manage-hire",
+        name: "manage-hire",
+        children: [
+          {
+            path: "post.do",
+            name: "post",
+            component: Post,
+          }
+        ],
+      },
       },      
     ],
   },
