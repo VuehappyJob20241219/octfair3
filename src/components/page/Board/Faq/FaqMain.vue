0b6d947405ddc3874757c1474c00c547c2dc29f7
hyeyum <top2480@naver.com>
2024-12-20 17:43:00 +0900

    faq 전체 조회 구현 완료/상세조회 구현 중

--- src/components/page/Board/Faq/FaqMain.vue
+++ src/components/page/Board/Faq/FaqMain.vue
@@ -1,4 +1,9 @@
-<template>  
+<template>
+  <FaqModal v-if="faqModalState.modalState"
+    @postSuccess="searchList"
+    @modalClose="() => (faqSeq = 0)"
+    :idx="faq_idx"
+  />
   <div class="divFaqList">
     <table>
       <colgroup>
@@ -17,18 +22,17 @@
         </tr>
       </thead>
       <tbody>
-        <template >...로딩중</template>
+        <template>...로딩중</template>
         <template v-if="faqList">
-          <template v-if="faqList.faqCnt>0" >
-            <tr v-for="faq in faqList.faq" :key="faq.faqInx"
-              @click="handlerModal(faq.faqInx)">
-              <td>{{ faq.faqIdx }}</td>
-              <td>{{ faq.title }}</td>
-              <td>{{ faq.createdDate.substr(0,10) }}</td>
+          <template v-if="faqList.faqCnt > 0">
+            <tr v-for="faq in faqList.faq" :key="faq.faq_idx" >
+              <td>{{ faq.faq_idx }}</td>
+              <td @click="handlerModal(faq.faq_idx)">{{ faq.title }}</td>
+              <td>{{ faq.created_date.substr(0,10)}}</td>
               <td>{{ faq.author }}</td>
             </tr>
           </template>
-          <template >
+          <template v-else>
             <tr>
               <td colspan="7">일치하는 검색 결과가 없습니다</td>
             </tr>
@@ -36,58 +40,62 @@
         </template>
       </tbody>
     </table>
-    <Pagination 
-            :totalItems="faqList?.faqCnt || 0"
-            :items-per-page="5"
-            :max-pages-shown="5"
-            :onClick="searchList"
-            v-model="cPage"
-        />
+    <Pagination
+      :totalItems="faqList?.faqCnt || 0"
+      :items-per-page="5"
+      :max-pages-shown="5"
+      :onClick="searchList"
+      v-model="cPage"
+    />
   </div>
 </template>
 <script setup>
-import axios from 'axios';
-import { onMounted } from 'vue';
-import { useRoute } from 'vue-router';
-import Pagination from '../../../common/Pagination.vue';
-import { useModalStore } from '../../../../stores/modalState';
+import axios from "axios";
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+import Pagination from "../../../common/Pagination.vue";
+import { useModalStore } from "../../../../stores/modalState";
+import { useUserInfo } from "../../../../stores/userInfo";
+import FaqModal from "./FaqModal.vue";
 
 const route = useRoute();
 const faqList = ref();
 const cPage = ref(1);
-const faqIdx = ref(0);
-const modalState = useModalStore();
+const faq_idx = ref(0);
+const faqModalState = useModalStore();
+const userInfo = useUserInfo();
 
-const searchList = async() => {
-    const param = new URLSearchParams({
-        searchTitle: '',
-        searchStDate:'',
-        searchEdDate: '',
-        currentPage: cPage.value,
-        pageSize:5
-    });   
-    console.log('넘겨주는 데이터',param);  
-    await axios.post('/api/board/faqListRe.do', param).then((res)=>{      
-      console.log(res.data);     
-        faqList.value = res.data;    
-        console.log(res.data);   
-    });
+const faq_fype = userInfo.user.userType === "B" ? 1 : 2;
+
+const searchList = async () => {
+  const param = new URLSearchParams({
+    searchTitle: route.query.searchTitle || "",
+    searchStDate: route.query.searchStDate || "",
+    searchEdDate: route.query.searchEdDate || "",
+    currentPage: cPage.value,
+    pageSize: 5,
+    faq_type: faq_fype,
+  }); 
+  await axios.post("/api/board/faqListRe.do", param).then((res) => {
+    faqList.value = res.data;    
+  });
+
 };
 
 const handlerModal = (idx) => {
-    console.log(idx);
-    modalState.setModalState();
-    faqIdx.value = idx;
+  console.log(typeof idx);
+  faqModalState.setModalState();
+  axios.post("/api/board/faqDetail.do", { faqSeq: idx }).then((res)=>{
+    faqList.value=res.data;
+  })
 };
 
-//watch(route, searchList);
+watch(route, searchList);
 
 onMounted(() => {
-    searchList();
+  searchList();
 });
-
 </script>
-
 
 <style lang="scss" scoped>
 table {
