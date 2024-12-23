526fcf5a19d600c21398cd0f3c5971c07f8efae8
jsmin6330 <jsmin6330@gmail.com>
2024-12-23 17:27:08 +0900

    비밀번호 수정 모달창 초기 구현

---
+++ src/components/page/Mypage/MypageModal.vue
@@ -0,0 +1,129 @@
+<template>
+    <teleport to="body">
+        <div class="backdrop">
+            <div class="container">
+                <label class="title">개인 회원 정보</label>
+                <!-- <button @click="pwReset">초기화</button> -->
+            </div>
+        </div>
+    </teleport>
+</template>
+
+
+<script setup>
+// import axios from "axios";
+import { useModalStore } from "../../../stores/modalState";
+
+const emit = defineEmits(["postSuccess", "modalClose"]);
+
+const modalStatePw = useModalStore();
+
+
+// const pwReset = () => {
+//     const param = new URLSearchParams({
+//         loginId: applicantDetail.value.loginId
+//     });
+
+//     axios.post("/api/manage-user/applicantPwReset.do", param)
+//         .then((res) => {
+//             console.log(res.data.result);
+//             if (res.data.result === "success") {
+//                 alert("비밀번호가 초기화되었습니다.");
+//             }
+//         })
+// }
+
+const handlerModal = () => {
+    modalStatePw.setModalState();
+};
+
+
+
+</script>
+
+
+<style lang="scss" scoped>
+.backdrop {
+    top: 0%;
+    left: 0%;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    display: flex;
+    flex-flow: row wrep;
+    justify-content: center;
+    align-items: center;
+    background: rgba(0, 0, 0, 0.5);
+    z-index: 1;
+    font-weight: bold;
+}
+
+.container {
+    background: white;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+    position: relative;
+    width: 400px;
+}
+
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+    margin: 20px 0px 0px 0px;
+    font-size: 18px;
+    text-align: left;
+
+    th,
+    td {
+        padding: 8px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+        text-align: center;
+    }
+
+    th {
+        background-color: #2676bf;
+        color: #ddd;
+    }
+
+    /* 테이블 올렸을 때 */
+    tbody tr:hover {
+        background-color: #d3d3d3;
+        opacity: 0.9;
+        cursor: pointer;
+    }
+}
+
+.button-box {
+    text-align: middle;
+    margin-top: 0px;
+}
+
+button {
+    background-color: #3bb2ea;
+    border: none;
+    color: white;
+    padding: 5px 5px;
+    text-align: right;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 1px 1px;
+    cursor: pointer;
+    border-radius: 12px;
+    box-shadow: 0 4px #999;
+    transition: 0.3s;
+
+    &:hover {
+        background-color: #45a049;
+    }
+
+    &:active {
+        background-color: #3e8e41;
+        box-shadow: 0 2px #666;
+        transform: translateY(2px);
+    }
+}
+</style>
