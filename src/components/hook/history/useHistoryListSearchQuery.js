dfe9762e839220d9052148a1571a7d00311e8b7a
shinyorrr <tmsgh21@gmail.com>
2024-12-24 17:27:21 +0900

    서치 리스트로 변경

--- src/components/hook/history/useHistoryListSearchQuery.js
+++ src/components/hook/history/useHistoryListSearchQuery.js
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/vue-query";
 import { historyListSearchApi } from "../../../api/history/historyListSearchApi";
 
-export const useHistoryListSearchQuery = (injectedValue, cPage) => {
+export const useHistoryListSearchQuery = ( cPage) => {
   return useQuery({
-    queryKey: ["historyList", injectedValue, cPage],
-    queryFn: () => historyListSearchApi(injectedValue.value, cPage.value),
+    queryKey: ["historyList",  cPage],
+    queryFn: () => historyListSearchApi( cPage.value),
     staleTime: 1000 * 60,
   });
 };
