ec1848666c0cb0a1da28330a238a1ff1171195d0
hykworld <hyk910@gamil.com>
2024-12-26 17:39:17 +0900

    이력서 작성 중 미리보기 css

--- src/components/page/Apply/ResumeDetail/ResumePreview.vue
+++ src/components/page/Apply/ResumeDetail/ResumePreview.vue
@@ -3,50 +3,249 @@
     <div class="backdrop" @click.self="closeModal">
       <div class="container">
         <div id="printArea">
-        <template v-if="resumeDetailinformation">
-          <template v-if="resumeDetailinformation.resIdx > 0">
-            <div v-for="[key, value] in Object.entries(resumeProperties)" :key="key">
-              <div v-if="value">
-                <span v-if="resumeBasicLabels[key]">{{ resumeBasicLabels[key] }}: {{ value }}</span>
+          <div class="resumePreviewTitle">이력서 미리보기</div>
+          <template v-if="resumeDetailinformation">
+            <div style="margin-bottom: 20px">
+              <p style="white-space: pre-line; font-size: 25px; font-weight: 900">{{ resumeProperties.resTitle }}</p>
+            </div>
+            <div style="margin-bottom: 5px" v-if="resumeProperties.userNm">
+              <p style="white-space: pre-line; font-size: 15px; font-weight: 500">
+                이름 : {{ resumeProperties.userNm }}
+              </p>
+            </div>
+            <div style="margin-bottom: 5px">
+              <p style="white-space: pre-line; font-size: 15px; font-weight: 500">
+                이메일 : {{ resumeProperties.email }}
+              </p>
+            </div>
+            <div style="margin-bottom: 10px">
+              <p style="white-space: pre-line; font-size: 15px; font-weight: 500">
+                연락처 : {{ resumeProperties.phone }}
+              </p>
+            </div>
+            <div style="margin-top: 20px; padding: 15px; border-top: 1px solid #ccc">
+              <div>
+                <p style="white-space: pre-line">{{ resumeProperties.shortIntro }}</p>
+              </div>
+              <div style="" v-if="resumeProperties.proLink">
+                <p style="white-space: pre-line">
+                  링크 : <a style="color: #36f" href="{{resumeProperties.proLink}}"></a>
+                </p>
+              </div>
+              <div style="" v-if="resumeProperties.fileName">
+                <p style="white-space: pre-line">첨부파일 :{{ resumeProperties.fileName }}</p>
+                파일다운로드 추가
               </div>
             </div>
-              <!-- 경력 -->
-              <div v-for="(career, index) in careerProperties" :key="index">
-                <div v-for="[key, value] in Object.entries(career)" :key="key">
-                  <div v-if="value">
-                    <span v-if="careerLabels[key]">{{ careerLabels[key] }} {{ value }}</span>
-                  </div>
-                </div>
-              </div>
-            <!-- 학력 -->
-              <div v-for="(edu, index) in eduProperties" :key="index">
-                <div v-for="[key, value] in Object.entries(edu)" :key="key">
-                  <div v-if="value">
-                    <span v-if="eduLabels[key]">{{ eduLabels[key] }} {{ value }}</span>
-                  </div>
-                </div>
-              </div>
-                <!-- 스킬 -->
-              <div v-for="(skill, index) in skillProperties" :key="index">
-                <div v-for="[key, value] in Object.entries(skill)" :key="key">
-                  <div v-if="value">
-                    <span v-if="skillLabels[key]">{{ skillLabels[key] }} {{ value }}</span>
-                  </div>
-                </div>
-              </div>
-             <!-- 외국어 -->
-              <div v-for="(certification, index) in certProperties" :key="index">
-                <div v-for="[key, value] in Object.entries(certification)" :key="key">
-                  <div v-if="value">
-                    <span v-if="certLabels[key]">{{ certLabels[key] }} {{ value }}</span>
-                  </div>
-                </div>
-              </div>
           </template>
-        </template>
-      </div>
-        <button class="btnType blue" @click="closeModal">닫기</button>
-        <button class="btnType gray" @click="printJS('printArea', 'html')">인쇄</button>
+          <!-- 경력 -->
+          <template v-if="careerProperties.length < 0">
+            <div style="padding: 15px">
+              <div
+                style="
+                  font-size: 1.2em;
+                  font-weight: bold;
+                  color: #333;
+                  padding: 10px;
+                  border-top: 1px solid #000;
+                  border-bottom: 1px solid #ccc;
+                "
+              >
+                경력
+              </div>
+              <div>
+                <ul style="list-style: none; padding: 0">
+                  <div v-for="(career, index) in careerProperties" :key="index">
+                    <li
+                      style="
+                        padding: 15px;
+                        border-bottom: 2px solid #f1f1f1;
+                        display: flex;
+                        align-items: flex-start;
+                        align-items: center;
+                      "
+                    >
+                      <!-- 날짜 데이터 -->
+                      <div style="flex: 1; text-align: center; font-weight: 800">
+                        <span>
+                          {{ career.startDate.substr(0, 7) }}
+                          ~
+                          {{ career.endDate.substr(0, 7) }}
+                        </span>
+                      </div>
+                      <!-- 나머지 데이터 -->
+                      <div style="flex: 4; padding-left: 10px">
+                        <div style="flex: 3; padding-left: 10px">
+                          <div style="display: flex; align-items: center; margin-bottom: 20px">
+                            <div style="font-weight: 900; font-size: 20px; margin-left: 5px">{{ career.company }}</div>
+                            <div style="font-weight: 900; margin-left: 10px">
+                              &nbsp; {{ career.dept }} | {{ career.position }}
+                            </div>
+                          </div>
+                        </div>
+                        <div style="margin-top: 20px; white-space: pre-line">
+                          <p>{{ career.crrDesc }}</p>
+                        </div>
+                      </div>
+                    </li>
+                  </div>
+                </ul>
+              </div>
+            </div>
+          </template>
+          <!-- 학력 -->
+          <template v-if="eduProperties.length < 0">
+            <div style="margin-top: 20px; padding: 15px">
+              <div
+                style="
+                  font-size: 1.2em;
+                  font-weight: bold;
+                  color: #333;
+                  padding: 10px;
+                  border-top: 1px solid #000;
+                  border-bottom: 1px solid #ccc;
+                "
+              >
+                학력
+              </div>
+              <div>
+                <ul style="list-style: none; padding: 0">
+                  <div v-for="(edu, index) in eduProperties" :key="index">
+                    <li
+                      style="
+                        padding: 15px;
+                        border-bottom: 2px solid #f1f1f1;
+                        display: flex;
+                        align-items: flex-start;
+                        align-items: center;
+                        display: flex;
+                      "
+                    >
+                      <div style="flex: 1; width: 20%; padding-left: 10px">
+                        <span style="font-weight: 900">
+                          <span>
+                            {{ edu.admDate.substr(0, 7) }}
+                            ~
+                            {{ edu.grdDate.substr(0, 7) }}
+                          </span>
+                        </span>
+                      </div>
+
+                      <div style="flex: 3; width: 60%; padding-left: 10px">
+                        <span style="font-weight: 900; font-size: 15px"> {{ edu.schoolName }} </span>
+                        <template v-if="edu.major">
+                          <span style="font-weight: 900"> | {{ edu.major }} </span>
+                        </template>
+                      </div>
+                      <div style="flex: 1; width: 20%; text-align: left; font-weight: 500">
+                        <span>{{ edu.grdStatus }} </span>
+                      </div>
+                    </li>
+                  </div>
+                </ul>
+              </div>
+            </div>
+          </template>
+          <!-- 스킬 -->
+          <template v-if="skillProperties.length < 0">
+            <div style="margin-top: 20px; padding: 15px">
+              <div
+                style="
+                  font-size: 1.2em;
+                  font-weight: bold;
+                  color: #333;
+                  padding: 10px;
+                  border-top: 1px solid #000;
+                  border-bottom: 1px solid #ccc;
+                "
+              >
+                스킬
+              </div>
+              <div>
+                <ul style="list-style: none; padding: 0">
+                  <div v-for="(skill, index) in skillProperties" :key="index">
+                    <li
+                      style="
+                        padding: 15px;
+                        border-bottom: 2px solid #f1f1f1;
+                        display: flex;
+                        align-items: flex-start;
+                        align-items: center;
+                        display: flex;
+                      "
+                    >
+                      <div style="flex: 1; width: 20%; padding-left: 10px">
+                        <span style="font-weight: 900">
+                          <span>
+                            {{ skill.skillName }}
+                          </span>
+                        </span>
+                      </div>
+                      <div style="flex: 3; width: 60%; padding-left: 10px">
+                        <span style="font-weight: 900; font-size: 15px"> {{ skill.skillDetail }} </span>
+                      </div>
+                    </li>
+                  </div>
+                </ul>
+              </div>
+            </div>
+          </template>
+          <!-- 외국어 -->
+          <template v-if="certProperties.length < 0">
+            <div style="margin-top: 20px; padding: 15px">
+              <div
+                style="
+                  font-size: 1.2em;
+                  font-weight: bold;
+                  color: #333;
+                  padding: 10px;
+                  border-top: 1px solid #000;
+                  border-bottom: 1px solid #ccc;
+                "
+              >
+                자격증 및 외국어
+              </div>
+              <div>
+                <ul style="list-style: none; padding: 0">
+                  <div v-for="(cert, index) in certProperties" :key="index">
+                    <li
+                      style="
+                        padding: 15px;
+                        border-bottom: 2px solid #f1f1f1;
+                        display: flex;
+                        align-items: flex-start;
+                        align-items: center;
+                        display: flex;
+                      "
+                    >
+                      <div style="flex: 1; width: 20%; padding-left: 10px">
+                        <span style="font-weight: 900">
+                          <span>
+                            {{ cert.acqDate.substr(0, 7) }}
+                          </span>
+                        </span>
+                      </div>
+
+                      <div style="flex: 2; width: 60%; padding-left: 10px">
+                        <span style="font-weight: 900; font-size: 15px"> {{ cert.certName }} </span>
+                      </div>
+                      <div style="flex: 1; width: 60%; padding-left: 10px">
+                        <span style="font-weight: 900"> {{ cert.grade }} </span>
+                      </div>
+                      <div style="flex: 1; width: 20%; text-align: left; font-weight: 500">
+                        <span>{{ cert.issuer }} </span>
+                      </div>
+                    </li>
+                  </div>
+                </ul>
+              </div>
+            </div>
+          </template>
+        </div>
+        <div class="button-container">
+          <button class="btnType blue" @click="closeModal">닫기</button>
+          <button class="btnType gray" @click="printJS('printArea', 'html')">인쇄</button>
+        </div>
       </div>
     </div>
   </teleport>
@@ -55,9 +254,9 @@
 <script setup>
 import { useModalStore } from "@/stores/modalState";
 import axios from "axios";
-import { Resume } from "../../../../api/axiosApi/resumeApi";
+import printJS from "print-js";
 import { computed } from "vue";
-import printJS from 'print-js'
+import { Resume } from "../../../../api/axiosApi/resumeApi";
 
 const modalState = useModalStore();
 const props = defineProps(["idx"]);
@@ -65,42 +264,6 @@ const resumeDetailinformation = ref({
   resIdx: 0,
   resumeInfo: {},
 });
-
-const resumeBasicLabels = {
-  resTitle: "제목",
-  userNm: "이름",
-  email: "이메일",
-  phone: "핸드폰",
-  shortIntro: "간단소개",
-  perStatement: "자기소개",
-  proLink: "링크",
-  fileName: "첨부파일",
-};
-
-const careerLabels = {
-  company: "회사명:",
-  dept:"근무부서:",
-  position: "직책/직급:",
-  startDate:"근무기간:",
-  endDate:"~",
-  reason: "퇴사사유:",
-  crrDesc: "담당업무:",
-};
-
-const eduLabels = {
-  eduLevel: "학력구분:",
-  schoolName:"학교명:",
-  major: "전공명",
-  admDate:"재학기간:",
-  grdDate:"~",
-  grdStatus: "졸업여부:",
-};
-
-const skillLabels = {
-  skillName: "스킬명:",
-  skillDetail: "스킬상세기재:",
-};
-
 const certLabels = {
   certName: "자격증명:",
   grade: "등급:",
@@ -171,14 +334,21 @@ input[type="text"] {
   border-radius: 4px;
   border: 1px solid #ccc;
 }
-
+.resumePreviewTitle {
+  background-color: #3e4463; /* 원하는 배경색 */
+  color: white; /* 글자 색상 변경 (선택 사항) */
+  padding: 5px; /* 내부 여백 */
+  width: 100%; /* 부모 요소에 맞게 넓이 설정 */
+  position: relative; /* 부모 요소에 독립적으로 위치 */
+  border-radius: 4px;
+}
 .container {
-  background: white;
+  background-color: white;
   padding: 20px;
   border-radius: 8px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
   position: relative;
-  width: 400px;
+  width: 600px;
 }
 
 img {
@@ -206,6 +376,13 @@ img {
   }
 }
 
+.button-container {
+  display: flex; /* Flexbox 사용 */
+  justify-content: center; /* 수평 중앙 정렬 */
+  gap: 10px; /* 버튼 사이 간격 (선택적) */
+  margin-top: 20px; /* 위쪽 여백 (선택적) */
+}
+
 .button-box {
   text-align: right;
   margin-top: 10px;
@@ -226,13 +403,55 @@ button {
   transition: 0.3s;
 
   &:hover {
-    background-color: #45a049;
+    background-color: #4583a0;
   }
 
   &:active {
-    background-color: #3e8e41;
+    background-color: #4583a0;
     box-shadow: 0 2px #666;
     transform: translateY(2px);
+  }
+
+  .btnType {
+    background-color: #3bb2ea; /* 기본 배경 색상 */
+    border: none;
+    color: white;
+    padding: 10px 22px;
+    text-align: right;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 12px;
+    box-shadow: 0 4px #999;
+    transition: 0.3s;
+
+    &:hover {
+      background-color: #4583a0;
+    }
+
+    &:active {
+      background-color: #4583a0;
+      box-shadow: 0 2px #666;
+      transform: translateY(2px);
+    }
+  }
+
+  .btnType.gray {
+    background-color: #6c757d; /* 회색 버튼 */
+  }
+
+  .btnType.blue {
+    background-color: #007bff; /* 파란색 버튼 */
+  }
+
+  .btnType.gray:hover {
+    background-color: #5a6268; /* 회색 버튼 호버 색상 */
+  }
+
+  .btnType.blue:hover {
+    background-color: #0056b3; /* 파란색 버튼 호버 색상 */
   }
 }
 </style>
