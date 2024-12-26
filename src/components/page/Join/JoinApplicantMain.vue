ec0e4064e19b6ef5196418ec4cc87a5462775dd8
jsmin6330 <jsmin6330@gmail.com>
2024-12-26 11:25:19 +0900

    디자인 적용

--- src/components/page/Join/JoinApplicantMain.vue
+++ src/components/page/Join/JoinApplicantMain.vue
@@ -29,6 +29,7 @@
                     <tr>
                         <th>성별</th>
                         <td><select v-model="register.sex">
+                                <option disabled value="">선택</option>
                                 <option value="1">남자</option>
                                 <option value="2">여자</option>
                             </select></td>
@@ -71,7 +72,9 @@ import { ref, watch } from 'vue';
 import { useRoute } from "vue-router";
 
 const route = useRoute();
-const register = ref({});
+const register = ref({
+    sex: ""
+});
 const checkId = ref(false);
 
 
@@ -212,3 +215,123 @@ watch(() => register.value.loginId, () => {
 })
 
 </script>
+
+<style lang="scss" scoped>
+.backdrop {
+    top: 0%;
+    left: 0%;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    display: flex;
+    flex-flow: row wrep;
+    justify-content: center;
+    align-items: center;
+    background: rgba(0, 0, 0, 0.5);
+    z-index: 999;
+    font-weight: bold;
+}
+
+label {
+    display: flex;
+    flex-direction: column;
+}
+
+label.title {
+    font-size: 18px;
+}
+
+.container {
+    background: white;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+    position: relative;
+    max-width: 60%;
+}
+
+input[type="text"],
+input[type="date"],
+input[type="email"],
+input[type="tel"] {
+    padding: 8px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+    // font-size: 13px;
+    width: 200px;
+}
+
+select {
+    padding: 8px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+}
+
+.button-box {
+    text-align: right;
+    margin-top: 0px;
+}
+
+button {
+    white-space: nowrap;
+    /* 줄 바꿈 방지 */
+    background-color: #3bb2ea;
+    border: none;
+    color: white;
+    padding: 10px 22px;
+    text-align: right;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 12px;
+    box-shadow: 0 4px #999;
+    transition: 0.3s;
+
+    &:hover {
+        background-color: #45a049;
+    }
+
+    &:active {
+        background-color: #3e8e41;
+        box-shadow: 0 2px #666;
+        transform: translateY(2px);
+    }
+}
+
+table {
+    border-collapse: collapse;
+    margin: 20px 0px 0px 0px;
+    font-size: 14px;
+    text-align: left;
+
+    th {
+        white-space: nowrap;
+        /* 줄 바꿈 방지 */
+        width: 100px;
+        background-color: #2676bf;
+        color: #ddd;
+        display: block;
+        height: 60px;
+        line-height: 60px;
+        padding-left: 10px;
+    }
+
+    td {
+        // padding: 8px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+        width: 100px;
+        height: 60px;
+    }
+
+    tbody {
+        width: 500px;
+    }
+}
+</style>
