e2fd4ff5b31a3e73cb610414449f86236d041bef
jsmin6330 <jsmin6330@gmail.com>
2024-12-27 13:59:01 +0900

    회원 탈퇴 시 세션의 userinfo 비우기

--- src/components/page/Mypage/WithdrawModal.vue
+++ src/components/page/Mypage/WithdrawModal.vue
@@ -47,6 +47,7 @@ const handlerBtn = () => {
                 alert("탈퇴 되었습니다.\n지금까지 서비스를 이용해주셔서 감사합니다.")
                 userInfo.setAuthenticated();
                 handlerModal();
+                sessionStorage.setItem("userInfo", "");
                 router.push('/');
             } else {
                 alert("비밀번호를 확인해주세요.")
