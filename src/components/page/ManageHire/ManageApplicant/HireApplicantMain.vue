70b639b25c0c7e300f45ba0f5f0f1996082c4204
youngchan <yckim8721@gmail.com>
2024-12-27 14:31:14 +0900

    기업상세,수정 페이지 css 변경

--- src/components/page/ManageHire/ManageApplicant/HireApplicantMain.vue
+++ src/components/page/ManageHire/ManageApplicant/HireApplicantMain.vue
@@ -1,17 +1,70 @@
 <template>
-  <div>
+  <div class="applicantList">
+    <!-- <NoticeModal
+      v-if="modalState.modalState"
+      @postSuccess="searchList"
+      @modalClose="() => (noticeIdx = 0)"
+      :idx="noticeIdx"
+    /> -->
+    현재 페이지: {{ cPage }} 총 개수: {{ ApplicantList?.ApplicantCount }}
     <table>
+      <colgroup>
+        <col width="10%" />
+        <col width="40%" />
+        <col width="20%" />
+        <col width="30%" />
+      </colgroup>
+
       <thead>
         <tr>
-          <th>지원자</th>
-          명
+          <th scope="col">번호</th>
+          <th scope="col">이력서 제목</th>
+          <th scope="col">지원자 이름</th>
+          <th scope="col">지원자 이메일</th>
         </tr>
       </thead>
+      <tbody>
+        <template v-if="ApplicantList">
+          <template v-if="ApplicantList.ApplicantCount > 0">
+            <tr v-for="list in ApplicantList.value" :key="list.resIdx">
+              <td>{{ list.resIdx }}</td>
+              <td>{{ list.resTitle }}</td>
+              <td>{{ list.name }}</td>
+              <td>{{ list.email }}</td>
+            </tr>
+          </template>
+          <template v-else>
+            <tr>
+              <td colspan="7">일치하는 검색 결과가 없습니다</td>
+            </tr>
+          </template>
+        </template>
+      </tbody>
     </table>
+    <!-- <Pagination
+      :totalItems="noticeList?.noticeCnt || 0"
+      :items-per-page="5"
+      :max-pages-shown="5"
+      :onClick="searchList"
+      v-model="cPage"
+    /> -->
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import axios from "axios";
+
+const searchList = () => {
+  const param = new URLSearchParams({});
+  axios.post("/api/manage-hire/applicantListBody.do").then((res) => {
+    ApplicantList.value = res.data.value;
+  });
+};
+
+onBeforeMount(() => {
+  searchList();
+});
+</script>
 
 <style lang="scss" scoped>
 table {
