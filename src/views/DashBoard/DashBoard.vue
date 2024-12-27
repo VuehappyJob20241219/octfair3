4e2f130a9f48672f2ebbde14c8782362d1e1debb
skh9301 <skh9301@naver.com>
2024-12-27 17:33:40 +0900

    유튜브 만드는중

--- src/views/DashBoard/DashBoard.vue
+++ src/views/DashBoard/DashBoard.vue
@@ -4,15 +4,26 @@
       <LeftManuBar></LeftManuBar>
     </li>
     <li class="content">
-      <keep-alive
-        ><router-view><video-youtube></video-youtube></router-view
-      ></keep-alive>
+      <keep-alive>
+        <div v-if="isHomePage">
+          <video-youtube />
+        </div>
+        <router-view> </router-view>
+      </keep-alive>
     </li>
   </ul>
 </template>
 
 <script setup>
+import { useRoute } from "vue-router";
 import LeftManuBar from "../../components/layout/LeftManuBar.vue";
+import { computed } from "vue";
+
+// 현재 경로를 가져오기
+const route = useRoute();
+
+// '/vue' 경로일 때만 YouTube 비디오를 보이도록
+const isHomePage = computed(() => route.path === "/vue");
 </script>
 
 <style scoped>
