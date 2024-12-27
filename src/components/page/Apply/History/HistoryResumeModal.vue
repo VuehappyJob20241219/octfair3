e01cb128b7a95a511aced5cb13c9d8ece1b11100
shinyorrr <tmsgh21@gmail.com>
2024-12-27 13:49:01 +0900

    .

---
+++ src/components/page/Apply/History/HistoryResumeModal.vue
@@ -0,0 +1,16 @@
+<template>
+  <ContextBox>이력서 미리보기</ContextBox>
+
+</template>
+
+<script setup>
+
+const searchDetail = () => {
+    // const result = axios.post('/api/apply/')
+}
+
+</script>
+
+<style>
+
+</style>
