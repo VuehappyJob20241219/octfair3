6d6b7436d44c3530a30db06ceb3e2afd9c1e78f6
psj <poker6711@naver.com>
2024-12-31 13:30:25 +0900

    0.0.0/2.4.1.6/psj

--- src/views/ManageHire/BizPost.vue
+++ src/views/ManageHire/BizPost.vue
@@ -1,13 +1,15 @@
 <template>
     <template v-if="userInfo.user.userType === 'M' && route.name == 'managePostApproval'">
         <ContextBox>공고심사</ContextBox>
+        <BizPostSearch></BizPostSearch>
     </template>
     <template v-if="userInfo.user.userType === 'A' || route.name == 'managePost'">
         <ContextBox>공고보기</ContextBox>
+        <BizPostSearch></BizPostSearch>
     </template>
     <template v-if="userInfo.user.userType === 'B'">
         <ContextBox>공고관리</ContextBox>
-    </template>    
+    </template>        
     <BizPostMain></BizPostMain>
 </template>
 
