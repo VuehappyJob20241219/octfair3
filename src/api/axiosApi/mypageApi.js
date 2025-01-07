0d82183b259c3ffcc21e55bae78b13622059ed9b
jsmin6330 <jsmin6330@gmail.com>
2025-01-07 09:29:32 +0900

    회원 탈퇴 모듈화 적용

--- src/api/axiosApi/mypageApi.js
+++ src/api/axiosApi/mypageApi.js
@@ -2,4 +2,5 @@ export const mypage = {
   userDetail: "/api/mypage/userDetail.do",
   userDetailUpdate: "/api/mypage/updateUserInfo.do",
   userPwUpdate: "/api/mypage/updatePwBCrypt.do",
+  withDraw: "/api/mypage/deleteUser.do",
 };
